339eb5eac6e0e066813d47f813878b7aed02ff5d
MammadaliMammadaliyev <mammadaliyevmammadali@gmail.com>
2025-09-15 20:15:57 +0400

    qaymag

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("Hello Roya")
+
+def main():
+    return NotImplemented
---
+++ shop.py
@@ -0,0 +1,4 @@
+class Shop:
+    def __init__(self, name, location) -> None:
+        self.name = name
+        self.location = location
